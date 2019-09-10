1dd650c2d2f8d70dd4fe0cb8b7ece38f2f65ca78
Aluno <Aluno@SN-304591>
2019-09-09 21:25:38 -0300

    Aula 09/09/2019 - PHP - LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@
 route::get('/home', 'IndexController@index');
 route::get('/contato', 'ContatoController@index');
 route::get('/produto', 'produtoController@produto');
-
 route::get('/Institucional', 'InstitucionalController@Institucional');
+route::get('/ClienteForm', 'ClienteFormController@ClienteForm');
+route::get('/CategoriaForm', 'CategoriaFormController@CategoriaForm');
+route::get('/ProdutoForm', 'ProdutoFormController@ProdutoForm');
+
+route::resource('categoria', 'CategoriaController');
